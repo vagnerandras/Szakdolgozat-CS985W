186c4946f529c13fa4989095231e42e0f28a91d4
vagnerandras <vagnerandrasb@gmail.com>
2025-05-20 20:23:38 +0200

    Update LIPA.R

--- LIPA.R
+++ LIPA.R
@@ -76,33 +76,3 @@ dotplot.phylo4d(
 dev.off()
 
 
-# === Compute Local Moran's I (LIPA) ===
-# Example: Compute LIPA for the CopyNumbers
-local_i_lifestyle <- lipaMoran(
-  phylo_data,
-  trait = "CopyNumbers",  # Replace with your trait of interest
-  prox.phylo = "nNodes",           # Use phylogenetic proximity based on nodes
-  as.p4d = TRUE                    # Return a phylo4d object for plotting
-)
-
-# === Extract p-values and Assign Colors ===
-# Extract p-values for nodes (CopyNumbers)
-points_col_lifestyle <- lipaMoran(
-  phylo_data,
-  trait = "CopyNumbers",
-  prox.phylo = "nNodes"
-)$p.value
-
-# Assign colors based on significance
-points_col_lifestyle <- ifelse(points_col_lifestyle < 0.05, "red", "black")  # Red for significant nodes
-
-# === Visualize Local Moran's I on Phylogeny (LifestyleTrait) ===
-pdf("/scratch/vandras/vax/CopyNumbers_LIPA_Plot.pdf", width = 10, height = 18)
-par(mar = c(12, 4, 4, 2), las = 2)
-dotplot.phylo4d(
-  local_i_lifestyle,
-  dot.col = points_col_lifestyle,   # Node colors based on significance
-  main = "Local Moran's I (LIPA) for CopyNumbers", cex = 0.5
-)
-dev.off()
-
